2ae75e47088e349f3a8bd20bdae83ea2097f37be
beta-lux <beta.underalpha@gmail.com>
2020-09-23 17:15:52 +0900

    add name

--- ASSN/ASSN1/beta.c
+++
@@ -1,6 +0,0 @@
-#include "bits.h"
-#include <stdio.h>
-
-int main(){
-    printf("%x\n", float_i2f(100));
-}
--- ASSN/ASSN1/bits.c
+++ ASSN/ASSN1/bits.c
@@ -1,7 +1,7 @@
 /* 
  * CS:APP Data Lab 
  * 
- * <Please put your name and userid here>
+ * 권민재 mzg00
  * 
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
