505a250bfdcbb87dd33d0d5a2d95781ccc43e01d
beta-lux <beta.underalpha@gmail.com>
2020-09-28 03:20:48 +0900

    hw1

--- Lab/test.c
+++ Lab/test.c
@@ -2,4 +2,15 @@
 // Created by 민재 on 2020/09/09.
 //
 
+#include <stdio.h>
+#include <assert.h>
 
+unsigned rotate_left(unsigned x, int n) {
+    unsigned w = sizeof(unsigned) << 3;
+    return (x << n) | (x >> (w - n));
+}
+
+int main(){
+    assert(rotate_left(0x12345678, 0) == 0x12345678);
+    return 0;
+}
